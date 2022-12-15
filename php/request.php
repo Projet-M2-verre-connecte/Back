ad41d01b07df7d8fe5d59d4d68b0114465289133
François CAILLET <65292635+CptFranck@users.noreply.github.com>
2022-12-15 14:57:30 +0100

    api Kylian

--- php/request.php
+++ php/request.php
@@ -26,9 +26,10 @@ function dbConnect(){
 }
 
 //----------------------------------------------------------------------------
-//--- dbRequestPatient ----------------------------------------------------------
+//--- dbRequestPatient -------------------------------------------------------
+//--- ROUTE : http://10.10.54.10/php/patient ---------------------------------
 //----------------------------------------------------------------------------
-// On récupère toutes les données d'un patient
+// On récupère id, name, surname, d'un seul patient
 
 function dbRequestPatient($db, $id) {
 
@@ -45,14 +46,19 @@ function dbRequestPatient($db, $id) {
 }
 
 //----------------------------------------------------------------------------
-//--- dbRequestPatients -------------------------------------------------------
+//--- dbRequestPatients ------------------------------------------------------
+//--- ROUTE : http://10.10.54.10/php/select_patients?id=1 --------------------
 //----------------------------------------------------------------------------
-// On récupère les patients pour un moniteur choisi
+// On récupère id, name, surname, des patients ayant le même observateur	
 
-function dbRequestPatients($db, $id) {
-    
+function dbRequestPatients($db,$id) {
+
     try {
-        $query = $db->prepare('SELECT p.id_patient, p.name, p.surname  FROM Patient AS p INNER JOIN link_to AS l ON p.id_patient = l.id_patient INNER JOIN Monitor as m ON m.id_monitor = l.id_monitor WHERE m.id_monitor = ?;');
+        $query = $db->prepare('SELECT p.id_patient, p.name, p.surname  
+            FROM Patient AS p 
+            INNER JOIN Link_to AS l ON p.id_patient = l.id_patient 
+            INNER JOIN Monitor as m ON m.id_monitor = l.id_monitor 
+            WHERE m.id_monitor = ?;');
         $query->execute(array($id));
         $response = $query->fetchAll(PDO::FETCH_ASSOC);
         return $response;
@@ -64,15 +70,65 @@ function dbRequestPatients($db, $id) {
 }
 
 //----------------------------------------------------------------------------
-//--- dbRequestDatas -------------------------------------------------------
+//--- dbRequestDatas ---------------------------------------------------------
+//--- ROUTE : http://10.10.54.10/php/database.php/select_datas?id=1 ----------
 //----------------------------------------------------------------------------
-// On récupère les datas relative à un patient
+// On récupère id, name, surname, age, weight, objective d'un seul patient
 
 function dbRequestDatas($db, $id) {
     
     try {
         $query = $db->prepare('SELECT * FROM Data WHERE id_patient = ? ;');
         $query->execute(array($id));
+        $response = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $response;
+    }
+    catch (PDOExecption $exception) {
+        error_log('Connection error: '.$exception->getMessage());
+        return false;
+    }
+}
+
+//----------------------------------------------------------------------------
+//--- dbRequestTodayData -----------------------------------------------------
+//--- ROUTE : http://10.10.54.10/php/today?id=1 ------------------------------
+//----------------------------------------------------------------------------
+// On récupère la date et le volume d'un patient en fonction de la date du jour	
+
+function dbRequestTodayData($db, $id) {
+    
+    try {
+        $query = $db->prepare('SELECT datetime,volume 
+            FROM Data 
+            INNER JOIN Patient ON Data.id_patient = Patient.id_patient
+            WHERE DATE(datetime) = DATE(NOW())
+            AND Data.id_patient = ?;');
+        $query->execute(array($id));
+        $response = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $response;
+    }
+    catch (PDOExecption $exception) {
+        error_log('Connection error: '.$exception->getMessage());
+        return false;
+    }
+}
+
+//----------------------------------------------------------------------------
+//--- dbRequestSpecificDateData ----------------------------------------------
+//--- ROUTE : http://10.10.54.10/php/dateX?id=1&date=2022-11-15 --------------
+//----------------------------------------------------------------------------
+// On récupère la date et le volume bu d'un patient en fonction d'une date spécifié
+
+
+function dbRequestSpecificDateData($db, $id, $date) {
+    
+    try {
+        $query = $db->prepare('SELECT datetime,volume 
+            FROM Data
+            INNER JOIN Patient ON Data.id_patient = Patient.id_patient
+            WHERE DATE(datetime) = :date
+            AND Data.id_patient = :id;');
+        $query->execute(array(':date' => $date, ':id' => $id));
         $response = $query->fetchAll(PDO::FETCH_ASSOC);
         return $response;
     }
